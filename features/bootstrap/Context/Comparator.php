b0e71bba47f9e6dbcfae28d77d374f051838e498
kevins chmitt <kevin.schmitt.upjv@gmail.com>
2022-01-01 23:20:39 +0100

    add equal function

---
+++ features/bootstrap/Context/Comparator.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace RayTracer\Tests\Context;
+
+
+class Comparator {
+    const EPSILON = 0.00001;
+
+    public static function equal($a, $b) : bool
+    {
+        if((abs($a) - abs($b)) < self::EPSILON) {
+            return true;
+        }
+
+        return false;
+    }
+}
